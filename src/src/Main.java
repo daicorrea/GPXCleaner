c7485942701b99036d1e8c226648a56d3e16ac0d
PCS20151 <cesar.silva@uniriotec.br>
2015-10-14 23:20:27 -0300

    Alt 1
    Alt 1

--- src/src/Main.java
+++ src/src/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main(String[] args){
 		Password passwrd = new Password();
 		passwrd.setPassword("12345678");
-		
+		if(verifiedPassword(passwrd)){
+			//TODO Senha forte else Senha Fraca
+		};
 	}
 }
