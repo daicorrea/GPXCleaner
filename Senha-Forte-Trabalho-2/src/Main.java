27dacf0acac8c1dccbb5e72513a0dbdece8d6ee1
danenws <daiane.ocorrea@gmail.com>
2015-11-02 19:33:07 -0200

    Organizacao-por-pastas
    Separando os trabalhos por pastas

---
+++ Senha-Forte-Trabalho-2/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Classe principal
+ */
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		String passwd = "2hAj5#mne-ix.86H";
+		System.out.println(CheckStrength.checkPasswordStrength(passwd));
+	}
+}
