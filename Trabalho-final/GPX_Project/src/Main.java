1fadba965d39a81bdb3b144fc98bbb25ddb3dc94
flippe <flipeguedes@gmail.com>
2015-12-13 01:38:55 -0200

    v1

---
+++ Trabalho-final/GPX_Project/src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		gpxParser reader = new gpxParser();
+		
+		reader.gpxReader("Century-2007-02-18.gpx");
+
+	}
+
+}
