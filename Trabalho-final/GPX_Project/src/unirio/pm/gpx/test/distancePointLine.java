135d86e2c76f4e57d2806acfbda0e21772d2027f
Felipe Quintanilha <flipeguedes@gmail.com>
2015-12-14 00:15:43 -0200

    caso de teste distancePointLine

---
+++ Trabalho-final/GPX_Project/src/unirio/pm/gpx/test/distancePointLine.java
@@ -0,0 +1,18 @@
+package unirio.pm.gpx.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class distancePointLine {
+
+	@Test
+	public void calculateDistance(){
+		
+		
+		distancePointLine result = new distancePointLine();
+		
+		result.calculateDistance(); //Trackpoint parameters ??
+	}
+
+}
