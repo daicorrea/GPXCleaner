bd3187c8f1e7d7988a4e0851daaea17f0c826dd5
daicorrea <daiane.ocorrea@gmail.com>
2017-04-30 14:21:43 -0300

    Cleaning the Project

--- Trabalho-final/GPXCleaner/src/unirio/pm/gpxcleaner/run/Main.java
+++
@@ -1,9 +0,0 @@
-package unirio.pm.gpxcleaner.run;
-import unirio.pm.gpxcleaner.test.*;
-
-public class Main {
-	public static void main(String[] args) {
-		ReadXMLFile lerTeste = new ReadXMLFile();
-		lerTeste.ReadXML();
-	}
-}
